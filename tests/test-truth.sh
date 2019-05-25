b5a11c1cac27e5d96e0ac0de38a0a0b810c036f2
thoelze1 <thoelze1@binghamton.edu>
2019-05-24 22:47:48 -0400

    remove training/query files after testing

--- tests/test-truth.sh
+++ tests/test-truth.sh
@@ -27,19 +27,22 @@ do
 done
 '
 
-for trainingSize in `seq 10000 20000 100000`;
+for trainingSize in `seq 10000 1000 15000`;
 do
-	for dimension in `seq 5 5 25`;
+	for dimension in `seq 5 5 10`;
 	do
 		rel1=$(./make_training_file $trainingSize $dimension 1)
 		abs1=$(realpath $rel1)
-		rel2=$(./make_query_file 1 $dimension 1 5)
+		rel2=$(./make_query_file 50 $dimension 1 100)
 		abs2=$(realpath $rel2)
+		echo "--------------------------------------"
 		../k-nn 8 $abs1 $abs2 results.dat
 		if ! ./verify-results $abs1 $abs2 results.dat; then
 			echo "FAILED WITH " $rel1 $rel2
 			exit -1
 		fi
+		rm $abs1
+		rm $abs2
 	done
 done
 
